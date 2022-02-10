f5d6cb5993d3ff27f0b10e71918ca12a0b77b3fe
alexmil-sk <alexmilweb@gmail.com>
2022-02-10 10:26:05 +0100

    start hw-3

--- _Lesson-2/scripts/types.js
+++
@@ -1,8 +0,0 @@
-export const reviewFND = ['здорово', 'круто нереально'];
-export var Genre;
-(function (Genre) {
-  Genre[Genre['Fantasy'] = 0] = 'Fantasy';
-  Genre[Genre['Adventure'] = 1] = 'Adventure';
-  Genre[Genre['Horror'] = 2] = 'Horror';
-  Genre[Genre['Thriller'] = 3] = 'Thriller';
-})(Genre || (Genre = {}));
